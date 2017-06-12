b768c3cd44b048953fdc44a8d35bd46d78bc9956
mlynnes <micaelalsassano@gmail.com>
2017-06-12 16:15:36 -0400

    Sign In feature working
    Adds ajax call for sign in to api.js
    Adds sign in event and click handler to events.js
    Adds console logs and error to test sign in succes/failure
    
    The next step will be implementing the sign out feature.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const store = require('../store.js')
+const store = require('../store.js')
 
 const signUpSuccess = (data) => {
   console.log('sign up was successful and data is:', data)
@@ -10,7 +10,18 @@ const signUpFailure = (error) => {
   console.error(error)
 }
 
+const signInSuccess = (response) => {
+  store.user = response.user
+  console.log('sign in was successful and response is:', response)
+}
+
+const signInFailure = (data) => {
+  console.log('error signing out and data is:', data)
+}
+
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure
 }
