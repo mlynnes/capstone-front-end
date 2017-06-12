a3593a230e3405b175a439e883913cd306895c80
mlynnes <micaelalsassano@gmail.com>
2017-06-12 12:09:02 -0400

    Initial Commit
    Adds all needed files for the front-end repository
    Also edited the config.js file to include production and development
    URLS for my project.

---
+++ assets/scripts/config.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    development: 'http://localhost:4741',
+    production: 'https://thawing-plateau-88644.herokuapp.com/'
+  }
+}
+
+module.exports = config
